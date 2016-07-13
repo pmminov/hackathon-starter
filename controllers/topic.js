99fef75d6c779079de7287d616d1f05223f54681
Pablo Mino <pmminov@gmail.com>
2016-07-13 17:31:32 +1000

    incorporated card changes to BE

--- controllers/topic.js
+++ controllers/topic.js
@@ -9,9 +9,17 @@ const Topic = require('../models/Topic')
 exports.index = (req, res) => {
   Story.find({ topic: 'gay marriage' }, (err, stories) => {
     if (err) return console.log(err)
+    const cards = []
+    stories.forEach(function (story) {
+      var card = {
+        title: story.title,
+        text: story.description
+      }
+      cards.push(card)
+    }, this)
     res.render('topic', {
       title: 'Topic',
-      stories
+      cards
     })
   })
 }
